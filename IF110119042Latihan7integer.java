d1a54f7e613b80b284dc84036c3ba28aa7f40b7b
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-10-20 13:41:40 +0700

    Add files via upload

---
+++ IF110119042Latihan7integer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+
+/**
+ *
+ * @author Stks
+ */
+public class IF110119042Latihan7integer {
+
+public static void main(String[] args){
+    byte b;
+    short s;
+    int i;
+    long l;
+    
+    b= 120;
+    s= 32767;
+    i= 1_000_000_000;
+    l= 90000000000L;
+    System.out.println("byte : " + b);
+    System.out.println("short: " + s);
+    System.out.println("int: " + i);
+    System.out.println("long : " + l);
+}    
+}
